a305ec238926bf7bee3a5e950818383f51656d44
subin <osb4469@naver.com>
2023-07-24 17:48:23 +0900

    FeedWrite

---
+++ frontend/mon_palette/src/components/Feed/FeedTags.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import './FeedTags.css';
+
+const FeedTags = () => {
+  const [tags, setTags] = useState('');
+  const [tagList, setTagList] = useState([]);
+
+  const handleChange = (e) => {
+    setTags(e.target.value);
+  };
+
+  const handleAddTag = () => {
+    setTagList((prevTags) => [...prevTags, tags]);
+    setTags('');
+  };
+
+  return (
+    <div className="feed-tags">
+      <div className="tag-input">
+        <textarea
+          value={tags}
+          onChange={handleChange}
+          placeholder="태그를 입력하세요..."
+        />
+        <button onClick={handleAddTag}>Add Tag</button>
+      </div>
+      <div className="tag-list">
+        {tagList.map((tag, index) => (
+          <div key={index} className="tag-item">
+            #{tag}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FeedTags;
