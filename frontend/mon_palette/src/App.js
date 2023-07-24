92ce14ee132861c13a3aa87e3bd2ed35df91f57e
subin <dltmdals1620@gmail.com>
2023-07-24 17:48:23 +0900

    FeedWrite

--- frontend/mon_palette/src/App.js
+++ frontend/mon_palette/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import FeedWrite from './components/Feed/FeedWrite'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <FeedWrite />
     </div>
   );
 }
