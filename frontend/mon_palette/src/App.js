72bc8f095f514b8703d0a2024240de85f9873fcd
정수완 <krkdhs6240@naver.com>
2023-07-25 13:45:18 +0900

    Chore: bottom 에 Link 추가 / Header css 수정 230725

--- frontend/mon_palette/src/App.js
+++ frontend/mon_palette/src/App.js
@@ -1,5 +1,6 @@
 import NavigationBarHeader from './NavigationBar/NavigationBarHeader';
 import NavigationBarBottom from './NavigationBar/NavigationBarBottom';
+import FeedMain from './Feed/FeedMain';
 import { Routes, Route } from 'react-router-dom'
 function App() {
 
@@ -7,6 +8,8 @@ function App() {
     <div className="App">
       <Routes>
         <Route path='/' element={[<NavigationBarHeader/>, <NavigationBarBottom />]}/>
+
+        <Route path='/feed/' element={[<NavigationBarHeader/>, <NavigationBarBottom />, <FeedMain />] }/>
       </Routes>
     </div>
   );
