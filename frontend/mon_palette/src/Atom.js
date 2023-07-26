ed1bf0def21dae084563afb1d25cc7d39b6ebb61
조은정 <ejjeongs@naver.com>
2023-07-26 15:55:16 +0900

    0726 merge

--- frontend/mon_palette/src/Atom.js
+++ frontend/mon_palette/src/Atom.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-import { atom } from "recoil"
-
-export const modalState = atom({
-  key: 'modalState',
-  default: false
-})
-=======
 import { atom } from "recoil";
 
 export const modalState = atom({
 	key: "modalState",
 	default: false,
 });
->>>>>>> dbcd68f (Fix : 머지오류수정)
--- frontend/mon_palette/src/index.js
+++ frontend/mon_palette/src/index.js
@@ -1,29 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
-import { BrowserRouter as Router } from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
-    <RecoilRoot>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </RecoilRoot>
-  </Router>
-=======
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
->>>>>>> dbcd68f (Fix : 머지오류수정)
+	<Router>
+		<RecoilRoot>
+			<React.StrictMode>
+				<App />
+			</React.StrictMode>
+		</RecoilRoot>
+	</Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
