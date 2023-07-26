415b712f07da8ceb59e329b29bbf8424945e5f5c
조은정 <dltmdals1620@gmail.com>
2023-07-26 15:55:16 +0900

    0726 merge

--- frontend/mon_palette/src/App.test.js
+++ frontend/mon_palette/src/App.test.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-=======
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
@@ -14,5 +5,4 @@ test("renders learn react link", () => {
 	render(<App />);
 	const linkElement = screen.getByText(/learn react/i);
 	expect(linkElement).toBeInTheDocument();
->>>>>>> dbcd68f (Fix : 머지오류수정)
 });
