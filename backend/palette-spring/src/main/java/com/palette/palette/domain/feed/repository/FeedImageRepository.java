eecd26afc263952e91f18a35c04f7724f15a5b62
ske1620 <dltmdals1620@gmail.com>
2023-07-26 17:00:14 +0900

    Feat : 피드 수정 기능 구현 1 (controller -> service)

--- backend/palette-spring/src/main/java/com/palette/palette/domain/feed/repository/FeedImageRepository.java
+++ backend/palette-spring/src/main/java/com/palette/palette/domain/feed/repository/FeedImageRepository.java
@@ -3,5 +3,9 @@ package com.palette.palette.domain.feed.repository;
 import com.palette.palette.domain.feed.entity.FeedImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FeedImageRepository extends JpaRepository<FeedImage, Long> {
+
+    List<FeedImage> findAllByFeedId(Long feedId);
 }
