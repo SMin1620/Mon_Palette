56f75cde88d817c67c5c03d1345620d939f76573
subin <osb4469@naver.com>
2023-07-26 17:44:52 +0900

    Feat : Search2랑 FeedWrite 수정

--- frontend/mon_palette/src/components/Feed/FeedCaption.js
+++ frontend/mon_palette/src/components/Feed/FeedCaption.js
@@ -3,15 +3,11 @@ import React, { useState } from 'react';
 const FeedCaption = () => {
   const [caption, setCaption] = useState('');
 
-  const handleChange = (e) => {
-    setCaption(e.target.value);
-  };
-
   return (
     <div className="feed-caption">
       <textarea
         value={caption}
-        onChange={handleChange}
+        onChange={(e) => setCaption(e.target.value)}
         placeholder="피드에 추가할 캡션을 입력하세요..."
       />
     </div>
