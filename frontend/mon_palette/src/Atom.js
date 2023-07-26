e1511b4f4419656a4b0e32bb6cdf629ca8273286
subin <dltmdals1620@gmail.com>
2023-07-26 15:07:02 +0900

    Fix merge conflicts

--- frontend/mon_palette/src/Atom.js
+++ frontend/mon_palette/src/Atom.js
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 import { atom } from "recoil"
 
 export const modalState = atom({
   key: 'modalState',
   default: false
 })
+=======
+import { atom } from "recoil";
+
+export const modalState = atom({
+	key: "modalState",
+	default: false,
+});
+>>>>>>> dbcd68f (Fix : 머지오류수정)
