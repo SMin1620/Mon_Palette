6e3a6735f56f770d62fce32548a67f3146a5864c
subin <osb4469@naver.com>
2023-07-26 14:48:46 +0900

    Fix merge conflicts

--- frontend/mon_palette/src/App.js
+++ frontend/mon_palette/src/App.js
@@ -2,13 +2,12 @@ import NavigationBarHeader from './NavigationBar/NavigationBarHeader';
 import NavigationBarBottom from './NavigationBar/NavigationBarBottom';
 import FeedMain from './Feed/FeedMain';
 import { Routes, Route } from 'react-router-dom'
-function App() {
 
+function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={[<NavigationBarHeader/>, <NavigationBarBottom />]}/>
-
         <Route path='/feed/' element={[<NavigationBarHeader/>, <NavigationBarBottom />, <FeedMain />] }/>
       </Routes>
     </div>
