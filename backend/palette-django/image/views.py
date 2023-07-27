984e8ceb3139589a520e72bdb56cf070a43c26a3
ske1620 <dltmdals1620@gmail.com>
2023-07-27 14:20:02 +0900

    Feat : 컬러 app 추가 및 이미지 모델 설계

--- backend/palette-django/image/views.py
+++ backend/palette-django/image/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.parsers import MultiPartParser, FormParser
 
-# Create your views here.
+from image.serializers import ImageSerializer
+from color.models import Color
+
+
+class ImageViewSet(mixins.CreateModelMixin,
+                   viewsets.GenericViewSet):
+    queryset = Color.objects.all()
+    serializer_class = ImageSerializer
+    permission_classes = [IsAuthenticated]
+    parser_classes = (MultiPartParser, FormParser)
+
