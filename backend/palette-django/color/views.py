c0f7fbb59d88dc4848ec30b73e297ed058dd8e97
ske1620 <dltmdals1620@gmail.com>
2023-07-27 14:28:58 +0900

    Feat : color 시리얼라이저, 뷰셋 구현

--- backend/palette-django/color/views.py
+++ backend/palette-django/color/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets, status
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from color.serializers import ColorSerializer
+from color.models import Color
+
+
+class ColorListDetailViewSet(mixins.ListModelMixin,
+                             mixins.RetrieveModelMixin,
+                             viewsets.GenericViewSet):
+    """
+    컬러 조회
+    """
+    lookup_url_kwarg = 'color_id'
+
+    queryset = Color.objects.all()
+    serializer_class = ColorSerializer
+    # permission_classes = [IsAuthenticated]
+
