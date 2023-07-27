f920e72816bff439b9e19ec87ed1226cf422e9cc
ske1620 <dltmdals1620@gmail.com>
2023-07-27 14:14:38 +0900

    Feat : 컬러 진다 이미지 app 추가 및 이미지 분석 기능 구현

--- backend/palette-django/image/apps.py
+++ backend/palette-django/image/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ColorConfig(AppConfig):
+class ImageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'color'
+    name = 'image'
