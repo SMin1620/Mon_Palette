5f5c2bf1b0144a31c9465d5f830cdc7bab6c7ae8
ske1620 <dltmdals1620@gmail.com>
2023-07-27 14:28:58 +0900

    Feat : color 시리얼라이저, 뷰셋 구현

--- backend/palette-django/color/models.py
+++ backend/palette-django/color/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Color(models.Model):
+    name = models.CharField('컬러 이름', max_length=100)
+
+    class Meta:
+        db_table = 'colors'
+
+    def __str__(self):
+        return self.name
