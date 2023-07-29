7d976e1832484477e7510c9fa884d1c3695d5cce
이승민 <ibb09@naver.com>
2023-07-30 01:43:24 +0900

    Feat : 피드의 댓글 생성 기능 구현

--- backend/palette-spring/src/main/java/com/palette/palette/domain/comment/controller/CommentController.java
+++ backend/palette-spring/src/main/java/com/palette/palette/domain/comment/controller/CommentController.java
@@ -1,6 +1,7 @@
 package com.palette.palette.domain.comment.controller;
 
 import com.palette.palette.common.BaseResponse;
+import com.palette.palette.domain.comment.dto.create.CommentCreateReqDto;
 import com.palette.palette.domain.comment.service.CommentService;
 import com.palette.palette.domain.user.repository.UserRepository;
 import com.palette.palette.domain.user.service.UserService;
@@ -10,10 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.nio.file.attribute.UserPrincipalNotFoundException;
 
@@ -58,6 +56,42 @@ public class CommentController {
 
             e.printStackTrace();
             return BaseResponse.error("댓글 목록 조회 실패");
+        }
+    }
+
+    /**
+     * 댓글 생성
+     */
+    @Operation(summary = "피드 댓글 생성")
+    @PostMapping("/{feedId}/comment")
+    public BaseResponse commentCreate(
+            @RequestParam("feedId") Long feedId,
+            @RequestBody CommentCreateReqDto request,
+            Authentication authentication
+    ) {
+        System.out.println("댓글 생성 컨트롤러");
+
+        try {
+            // 인가된 사용자 정보
+            UserDetails userDetails = ((UserDetails) authentication.getPrincipal());
+
+            System.out.println("Controller userDetails >>> " + userDetails);
+            Long currentUserId = userRepository.findByEmail(userDetails.getUsername()).get().getId();
+            System.out.println("Controller user id>>> " + currentUserId);
+
+            // 유저 예외처리 :: 예외처리 커스텀 필요
+            if (currentUserId == null) {
+                throw new UserPrincipalNotFoundException("유효한 사용자가 아닙니다.");
+            }
+
+            // 피드 생성
+            commentService.commentCreate(request, feedId, currentUserId);
+
+            return BaseResponse.success(true);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return BaseResponse.error("피드 댓글 생성 실패");
         }
     }
 }
--- backend/palette-spring/src/main/java/com/palette/palette/domain/comment/service/CommentService.java
+++ backend/palette-spring/src/main/java/com/palette/palette/domain/comment/service/CommentService.java
@@ -1,12 +1,20 @@
 package com.palette.palette.domain.comment.service;
 
+import com.palette.palette.domain.comment.dto.create.CommentCreateReqDto;
 import com.palette.palette.domain.comment.dto.list.CommentListResDto;
+import com.palette.palette.domain.comment.entity.Comment;
 import com.palette.palette.domain.comment.repository.CommentRepository;
+import com.palette.palette.domain.feed.entity.Feed;
+import com.palette.palette.domain.feed.repository.FeedRepository;
+import com.palette.palette.domain.user.entity.User;
+import com.palette.palette.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.webjars.NotFoundException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,13 +22,16 @@ import java.util.stream.Collectors;
 @Service
 @Slf4j
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final FeedRepository feedRepository;
+    private final UserRepository userRepository;
 
 
     /**
-     * 피드 목록 조회
+     * 댓글 목록 조회
      * @param feedId
      */
     public List<CommentListResDto> commentList(Long feedId, int page, int size) {
@@ -30,5 +41,23 @@ public class CommentService {
         return commentRepository.findAllByComment(pageable, feedId).stream()
                 .map(CommentListResDto::toDto)
                 .collect(Collectors.toList());
+    }
+
+    /**
+     * 댓글 생성
+     * @param request
+     * @param feedId
+     */
+    @Transactional
+    public void commentCreate(CommentCreateReqDto request, Long feedId, Long userId) {
+
+        Feed feed = feedRepository.findById(feedId)
+                .orElseThrow(() -> new NotFoundException("피드를 찾을 수 없습니다."));
+
+        User user =  userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException("현재 사용자를 찾을 수 없습니다."));
+
+        Comment comment = Comment.toEntity(request, feed, user);
+        commentRepository.save(comment);
     }
 }
