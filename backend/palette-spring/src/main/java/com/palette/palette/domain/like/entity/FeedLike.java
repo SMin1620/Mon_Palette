0535140e63d48227eb8332deb12c8b79f6ebf226
이승민 <ibb09@naver.com>
2023-07-29 14:12:22 +0900

    Rename : 엔티티 디렉토리 구조 변경

--- backend/palette-spring/src/main/java/com/palette/palette/domain/like/entity/FeedLike.java
+++ backend/palette-spring/src/main/java/com/palette/palette/domain/like/entity/FeedLike.java
@@ -9,7 +9,7 @@ import lombok.*;
 @Setter
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "feed_likes")
 public class FeedLike {
