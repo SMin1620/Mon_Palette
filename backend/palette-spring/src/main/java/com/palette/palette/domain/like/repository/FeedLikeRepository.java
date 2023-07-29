b498ee49a41944231a666917544d4afc75638f07
이승민 <dltmdals1620@gmail.com>
2023-07-30 00:29:31 +0900

    Feat : 피드 좋아요한 사용자 목록 조회 기능 구현

--- backend/palette-spring/src/main/java/com/palette/palette/domain/like/repository/FeedLikeRepository.java
+++ backend/palette-spring/src/main/java/com/palette/palette/domain/like/repository/FeedLikeRepository.java
@@ -4,6 +4,7 @@ import com.palette.palette.domain.feed.entity.Feed;
 import com.palette.palette.domain.like.entity.FeedLike;
 import com.palette.palette.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,4 +14,7 @@ public interface FeedLikeRepository extends JpaRepository<FeedLike, Long> {
     Optional<FeedLike> findByFeedAndUser(Feed feed, User user);
 
     List<FeedLike> findAllByFeed(Feed feed);
+
+    @Query("select fi.user from FeedLike fi where fi.feed.id = :feedId")
+    List<User> findAllByUser(Long feedId);
 }
