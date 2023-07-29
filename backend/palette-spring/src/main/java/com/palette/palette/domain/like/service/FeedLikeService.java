8e87d4eae91247f6eef8c7e3857e01b381b65749
이승민 <dltmdals1620@gmail.com>
2023-07-29 14:56:14 +0900

    Feat : 좋아요 기능 구현

---
+++ backend/palette-spring/src/main/java/com/palette/palette/domain/like/service/FeedLikeService.java
@@ -0,0 +1,54 @@
+package com.palette.palette.domain.like.service;
+
+import com.palette.palette.domain.feed.entity.Feed;
+import com.palette.palette.domain.feed.repository.FeedRepository;
+import com.palette.palette.domain.like.entity.FeedLike;
+import com.palette.palette.domain.like.repository.FeedLikeRepository;
+import com.palette.palette.domain.user.entity.User;
+import com.palette.palette.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.webjars.NotFoundException;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Slf4j
+public class FeedLikeService {
+
+    private final UserRepository userRepository;
+    private final FeedRepository feedRepository;
+    private final FeedLikeRepository feedLikeRepository;
+
+
+    /**
+     * 피드 좋아요
+     */
+    @Transactional
+    public void feedLike(Long feedId, Long userId) throws Exception {
+
+        // 유저 유효성 검사
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException("사용자가 없습니다."));
+
+        // 피드 유효성 검사
+        Feed feed = feedRepository.findById(feedId)
+                .orElseThrow(() -> new NotFoundException("피드가 없습니다."));
+
+
+        // 이미 좋아요 되어있는지 체크
+        if (feedLikeRepository.findByFeedAndUser(feed, user).isPresent()) {
+            throw new Exception("이미 좋아요 되었습니다.");
+        }
+
+        FeedLike feedLike = FeedLike.builder()
+                .feed(feed)
+                .user(user)
+                .build();
+
+        feedLikeRepository.save(feedLike);
+    }
+
+}
