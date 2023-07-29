6cdb4ffba0dc049862209300376f6111d2f7c000
이승민 <dltmdals1620@gmail.com>
2023-07-29 14:12:22 +0900

    Rename : 엔티티 디렉토리 구조 변경

---
+++ backend/palette-spring/src/main/java/com/palette/palette/domain/like/controller/FeedLikeController.java
@@ -0,0 +1,109 @@
+package com.palette.palette.domain.like.controller;
+
+
+import com.palette.palette.common.BaseResponse;
+import com.palette.palette.domain.feed.service.FeedService;
+import com.palette.palette.domain.user.repository.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+
+@RestController
+@RequestMapping("/api/feed")
+@RequiredArgsConstructor
+@Slf4j
+@Tag(name = "피드 좋아요 API")
+public class FeedLikeController {
+
+    private final UserRepository userRepository;
+    private final FeedService feedService;
+
+
+    @Operation(summary = "좋아요 추가")
+    @PostMapping("/{id}/like")
+    public BaseResponse like(
+            @RequestParam("feedId") Long feedId,
+            Authentication authentication
+    ) {
+        try {
+            // 인가된 사용자 정보
+            UserDetails userDetails = ((UserDetails) authentication.getPrincipal());
+
+            System.out.println("Controller userDetails >>> " + userDetails);
+            Long currentUserId = userRepository.findByEmail(userDetails.getUsername()).get().getId();
+            System.out.println("Controller user id>>> " + currentUserId);
+
+            // 유저 예외처리 :: 예외처리 커스텀 필요
+            if (currentUserId == null) {
+                throw new UserPrincipalNotFoundException("유효한 사용자가 아닙니다.");
+            }
+
+            // 게시글 작성자의 이메일 가져옴
+            Long feedUserId = feedService.getFeedUserId(feedId);
+
+            System.out.println("feedUserId >>> " + feedUserId);
+
+            // 작성자와 현재 유저가 일치한지 처리하는 로직
+            if (! currentUserId.equals(feedUserId)) {
+                throw new UserPrincipalNotFoundException("작성자와 현재 사용자가 일치하지 않습니다.");
+            }
+
+            // 좋아요 비즈니스 로직
+
+
+            return BaseResponse.success(true);
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            return BaseResponse.error("좋아요 실패");
+        }
+    }
+
+
+    @Operation(summary = "좋아요 취소")
+    @DeleteMapping("/{id}/like")
+    public BaseResponse unLike(
+            @RequestParam("feedId") Long feedId,
+            Authentication authentication
+    ) {
+        try {
+
+            // 인가된 사용자 정보
+            UserDetails userDetails = ((UserDetails) authentication.getPrincipal());
+
+            System.out.println("Controller userDetails >>> " + userDetails);
+            Long currentUserId = userRepository.findByEmail(userDetails.getUsername()).get().getId();
+            System.out.println("Controller user id>>> " + currentUserId);
+
+            // 유저 예외처리 :: 예외처리 커스텀 필요
+            if (currentUserId == null) {
+                throw new UserPrincipalNotFoundException("유효한 사용자가 아닙니다.");
+            }
+
+            // 게시글 작성자의 이메일 가져옴
+            Long feedUserId = feedService.getFeedUserId(feedId);
+
+            System.out.println("feedUserId >>> " + feedUserId);
+
+            // 작성자와 현재 유저가 일치한지 처리하는 로직
+            if (!currentUserId.equals(feedUserId)) {
+                throw new UserPrincipalNotFoundException("작성자와 현재 사용자가 일치하지 않습니다.");
+            }
+
+            // 좋아요 취소 비즈니스 로직
+
+
+            return BaseResponse.success(true);
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            return BaseResponse.error("좋아요 취소 실패");
+        }
+    }
+}
