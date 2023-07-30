9402094a8d438ec49fce940ad15e1e3499d8d753
subin <osb4469@gmail.com>
2023-07-31 07:20:09 +0900

    Feat : 로딩 페이지 및 결과 페이지 1차 완성

--- frontend/mon_palette/src/PersonalColor/LoadingPage.js
+++ frontend/mon_palette/src/PersonalColor/LoadingPage.js
@@ -4,11 +4,13 @@ import styles from './LoadingPage.module.css';
 const LoadingPage = () => {
   return (
     <div className={styles.container}>
+      <div className={styles.textcontainer}>
       <div className={styles.text}>Please</div>
       <div className={styles.text}>wait</div>
       <div className={styles.text}>for</div>
       <div className={styles.text}>your</div>
       <div className={styles.text}>Palette</div>
+      </div>
       <div className={styles.spinner}></div>
     </div>
   );
--- frontend/mon_palette/src/PersonalColor/ResultPage.js
+++ frontend/mon_palette/src/PersonalColor/ResultPage.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import styles from './ResultPage.module.css';
+import { Link } from 'react-router-dom'
+
+const BackendResponseExample = () => {
+  // 예시로 받아온 값
+  const responseValue = 'spring warm';
+  // 받아온 값을 공백으로 분리하여 배열로 저장
+  const responseArray = responseValue.split(' ');
+
+  return (
+    <div className={styles['backend-response-container']}>
+      {responseArray.map((word, index) => (
+        <div key={index} className={styles['backend-response']}>
+            {word}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const BackendResponseDescription = () => {
+  return <div className={styles['result-description']}>
+        Additional information and description about the result can go here.
+        Jelly sweet roll jelly beans biscuit pie macaroon chocolate donut. Carrot cake caramels pie sweet apple pie tiramisu carrot cake. Marzipan marshmallow croissant tootsie roll lollipop. Cupcake lemon drops bear claw gummies. Jelly bear claw gummi bears lollipop cotton candy gummi bears chocolate bar cake cookie. Cupcake muffin danish muffin cookie gummies. Jelly beans tiramisu pudding. Toffee soufflé chocolate cake pastry brownie. Oat cake halvah sweet roll cotton candy croissant lollipop. Macaroon tiramisu chocolate bar candy candy carrot cake jelly sweet. Gummies croissant macaroon dessert. Chocolate cake dragée pie.
+        Next level tbh everyday carry, blog copper mug forage kitsch roof party pickled hammock kale chips tofu. Etsy shoreditch 8-bit microdosing, XOXO viral butcher banh mi humblebrag listicle woke bicycle rights brunch before they sold out ramps. Twee shabby chic taiyaki flannel, enamel pin venmo vape four loko. Hexagon kale chips typewriter kitsch 8-bit organic plaid small batch keffiyeh ethical banh mi narwhal echo park cronut.
+      </div>
+};
 
 const ResultPage = () => {
-    return <div>
-        <h2>summer cool</h2>
+  return (
+    <div className={styles.container}>
+      <div className={styles.page1}>
+        <div className={styles['text-container']}>
+          <div className={styles.text}>Your</div>
+          <div className={styles.text}>Palette</div>
+          <div className={styles.text}>is...</div>
+        </div>
+        <div >
+          <BackendResponseExample/>
+        </div>
+      </div>
+      <div className={styles['backend-response-scroll']}>
+        <BackendResponseExample />
+        <BackendResponseDescription />
+        <Link className={styles.button}>
+          <button className={styles.button2}>
+            Restart
+          </button>
+        </Link>
+      </div>  
     </div>
+  );
 };
+
 
 export default ResultPage;
