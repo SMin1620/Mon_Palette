82e26075c2516de1b0d65e392435f57646fc525d
이승민 <dltmdals1620@gmail.com>
2023-08-01 00:04:45 +0900

    Feat : 챌린지 좋아요 취소 추가 기능 구현

--- backend/palette-spring/src/main/java/com/palette/palette/domain/like/controller/ChallengeLikeController.java
+++ backend/palette-spring/src/main/java/com/palette/palette/domain/like/controller/ChallengeLikeController.java
@@ -12,10 +12,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.nio.file.attribute.UserPrincipalNotFoundException;
 
@@ -64,6 +61,41 @@ public class ChallengeLikeController {
 
             e.printStackTrace();
             return BaseResponse.error("좋아요 실패");
+        }
+    }
+
+    @Operation(summary = "챌린지 좋아요 취소")
+    @DeleteMapping("/{id}/like")
+    public BaseResponse challengeUnLike(
+            @PathVariable("id") Long challengeId,
+            HttpServletRequest request
+    ) {
+        System.out.println("챌린지 좋아요 취소 컨트롤러");
+
+        try {
+            //////////////////////// 토큰으로 인가된 사용자 정보 처리하는 로직
+            String token = jwtTokenProvider.resolveToken(request);
+            jwtTokenProvider.validateToken(token);
+
+            System.out.println("token >>> " + token);
+
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+            Long currentUserId = userRepository.findByEmail(userDetails.getUsername()).get().getId();
+
+            // 유저 예외처리 :: 예외처리 커스텀 필요
+            if (currentUserId == null) {
+                throw new UserPrincipalNotFoundException("유효한 사용자가 아닙니다.");
+            }
+
+            challengeLikeService.unLike(challengeId, currentUserId);
+
+            return BaseResponse.success(true);
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            return BaseResponse.error("좋아요 취소 실패");
         }
     }
 }
