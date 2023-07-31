c0ecce83993ded961f4c3b8f0c1b8c823b0327e1
subin <osb4469@naver.com>
2023-07-31 14:26:26 +0900

    Design : Search2 1차 최종 완성(백 연결 제외)

--- frontend/mon_palette/src/Search/Search2.js
+++ frontend/mon_palette/src/Search/Search2.js
@@ -3,13 +3,17 @@ import React from 'react';
 import SearchInput from './SearchInput';
 import RecentSearches from './RecentSearches';
 import styles from './Search2.module.css'
+import TrendingSearches from './TrendingSearches';
 
 const Search2 = () => {
   return (
+    <div className={styles.container}>
     <div className={styles['search-container']}>
       <SearchInput />
-      <RecentSearches
-      />
+    </div>
+      <hr className={styles['line']} />
+      <RecentSearches />
+      <TrendingSearches />
     </div>
   );
 };
