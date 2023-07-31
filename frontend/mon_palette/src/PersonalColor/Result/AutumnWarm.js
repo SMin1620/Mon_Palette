4bc88c7d46c7c2b97de2c1c36d47e85a541a7129
subin <dltmdals1620@gmail.com>
2023-07-31 17:54:30 +0900

    Feat : 퍼스널컬러 결과 화면

---
+++ frontend/mon_palette/src/PersonalColor/Result/AutumnWarm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './AutumnWarm.module.css';
+
+const AutumnWarm = () => {
+    return (
+        <div>AutumnWarm</div>
+    );
+};
+
+export default AutumnWarm;
--- frontend/mon_palette/src/PersonalColor/StartPage.js
+++ frontend/mon_palette/src/PersonalColor/StartPage.js
@@ -1,33 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import LoadingPage from './LoadingPage';
-import ResultPage from './ResultPage';
+import SpringWarm from './Result/SpringWarm';
+import SummerCool from './Result/SummerCool';
+import AutumnWarm from './Result/AutumnWarm';
+import WinterCool from './Result/WinterCool';
 import styles from './StartPage.module.css';
 
 const StartPage = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [showResultPage, setShowResultPage] = useState(false);
   const [uploadedImage, setUploadedImage] = useState(null);
+  const [season, setSeason] = useState(null);
   
   const handleStartLoading = () => {
     if (!uploadedImage) {
-      // 이미지를 업로드하지 않았을 경우에는 검사를 시작하지 않음
       return;
     }
     setIsLoading(true);
-    // 여기에 나중에 백엔드와 통신하는 코드를 넣을 거임.
-    setTimeout(() => {
-      setIsLoading(false);
-      setShowResultPage(true);
-    }, 3000);
-  };
+    // 여기서 나중에 백엔드와 통신하는 코드를 수행
+    // 통신이 완료된 후 결과를 받아올 것이라고 가정하겠습니다.
+    // 이 결과로 'season' 값을 결정하고 'showResultPage' 상태를 설정합니다.
+    const responseFromBackend = 'summer'; // 실제 API 응답값으로 대체해야 합니다.
 
+    // 통신이 완료되었으므로 결과값을 활용하여 상태를 업데이트합니다.
+    setSeason(responseFromBackend);
+    setIsLoading(false);
+    setShowResultPage(true);
+  };
+  
   const handleImageUpload = (e) => {
     const file = e.target.files[0];
     setUploadedImage(file);
   };
 
   const handleButtonClick = () => {
-    // 버튼을 눌렀을 때 파일 선택 창 열기
     document.getElementById('file-input').click();
   };
 
@@ -38,7 +44,7 @@ const StartPage = () => {
           {uploadedImage ? (
             <img className={styles.image} src={URL.createObjectURL(uploadedImage)} alt="Uploaded" />
           ) : (
-            <div className={styles['text-container']}>      
+            <div className={styles['text-container']}>
               <div className={styles.text}>What's</div>
               <div className={styles.text}>your</div>
               <div className={styles.text}>palette?</div>
@@ -52,7 +58,7 @@ const StartPage = () => {
             type="file"
             accept="image/*"
             onChange={handleImageUpload}
-            style={{ display: 'none' }} 
+            style={{ display: 'none' }}
           />
           <button className={styles.button2} onClick={handleStartLoading} disabled={!uploadedImage}>
             Start
@@ -60,7 +66,10 @@ const StartPage = () => {
         </div>
       )}
       {isLoading && <LoadingPage />}
-      {showResultPage && <ResultPage />}
+      {season === 'spring' && <SpringWarm />}
+      {season === 'summer' && <SummerCool />}
+      {season === 'autumn' && <AutumnWarm />}
+      {season === 'winter' && <WinterCool />}
     </div>
   );
 };
