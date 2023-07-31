b9a15e04db78d8b9e592a796c3d93ae6acc0f417
ske1620 <dltmdals1620@gmail.com>
2023-07-31 16:13:06 +0900

    Feat : 챌린지 수정 비즈니스 로직 구현

--- backend/palette-spring/src/main/java/com/palette/palette/domain/challenge/controller/ChallengeController.java
+++ backend/palette-spring/src/main/java/com/palette/palette/domain/challenge/controller/ChallengeController.java
@@ -2,6 +2,7 @@ package com.palette.palette.domain.challenge.controller;
 
 import com.palette.palette.common.BaseResponse;
 import com.palette.palette.domain.challenge.dto.create.ChallengeCreateReqDto;
+import com.palette.palette.domain.challenge.repository.ChallengeRepository;
 import com.palette.palette.domain.challenge.service.ChallengeService;
 import com.palette.palette.domain.user.entity.User;
 import com.palette.palette.domain.user.repository.UserRepository;
@@ -29,6 +30,7 @@ import java.nio.file.attribute.UserPrincipalNotFoundException;
 public class ChallengeController {
 
     private final ChallengeService challengeService;
+    private final ChallengeRepository challengeRepository;
     private final UserRepository userRepository;
     private final JwtTokenProvider jwtTokenProvider;
 
@@ -129,6 +131,58 @@ public class ChallengeController {
         } catch (Exception e) {
             e.printStackTrace();
             return BaseResponse.error("챌린지 상세 조회 실패");
+        }
+    }
+
+    /**
+     * 챌린지 수정
+     */
+    @Operation(summary = "챌린지 수정")
+    @PutMapping("/{id}")
+    public BaseResponse challengeUpdate(
+            @PathVariable("id") Long challengeId,
+            @RequestBody ChallengeCreateReqDto challengeCreateReqDto,
+            HttpServletRequest request
+    ) {
+        System.out.println("챌린지 수정 컨트롤러");
+
+        try {
+            //////////////////////// 토큰으로 인가된 사용자 정보 처리하는 로직
+            String token = jwtTokenProvider.resolveToken(request);
+            jwtTokenProvider.validateToken(token);
+
+            System.out.println("token >>> " + token);
+
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+            Long currentUserId = userRepository.findByEmail(userDetails.getUsername()).get().getId();
+
+            // 유저 예외처리 :: 예외처리 커스텀 필요
+            if (currentUserId == null) {
+                throw new UserPrincipalNotFoundException("유효한 사용자가 아닙니다.");
+            }
+
+            // 챌린지 작성자의 이메일을 가져옴
+            Long challengeUserId = challengeRepository.findByChallengeUserId(challengeId);
+
+            if (! currentUserId.equals(challengeUserId)) {
+                throw new UserPrincipalNotFoundException("작성자와 현재 사용자가 일치하지 않습니다.");
+            }
+
+            // 데이터 수정
+            ChallengeCreateReqDto dto = ChallengeCreateReqDto.builder()
+                    .video(challengeCreateReqDto.getVideo())
+                    .content(challengeCreateReqDto.getContent())
+                    .build();
+
+            challengeService.update(dto, challengeId);
+
+            return BaseResponse.success(true);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return BaseResponse.error("챌린지 수정 실패");
         }
     }
 }
