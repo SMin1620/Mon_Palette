3d82a9913161f7184d2d7f3d6d13c0cf5e3a7612
subin <dltmdals1620@gmail.com>
2023-08-03 08:29:44 +0900

    Feat : 검색 기능 1차 완성

--- frontend/mon_palette/src/Search/SearchInput.js
+++ frontend/mon_palette/src/Search/SearchInput.js
@@ -1,51 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
-import { recentSearchesState } from './Atom';
+import { recentSearchesState, searchQueryState } from './Atom';
 import styles from './SearchInput.module.css'
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import ArrowCircleLeftOutlinedIcon from '@mui/icons-material/ArrowCircleLeftOutlined';
 import axios from 'axios';
 import { loginState } from '../user/components/Atom';
 import { resultsState } from './Atom';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import Autocomplete from './AutoComplete';
 
 const SearchInput = () => {
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useRecoilState(searchQueryState);
   const Authorization = useRecoilValue(loginState);
   const [recentSearches, setRecentSearches] = useRecoilState(recentSearchesState);
   const [results, setResults] = useRecoilState(resultsState);
   const location = useLocation();
   const [fromSearchResults, setFromSearchResults] = useState(false);
   const [suggestions, setSuggestions] = useState([]);
+  const navigate = useNavigate();
 
+  const handleSearch = (query) => {
+    setResults({});
+    setRecentSearches((prevSearches) => [...prevSearches, query]);
+    console.log(`${query}로 검색을 수행합니다.`)
+  
+    axios.get(
+      `${process.env.REACT_APP_API}/api/search?page=0&type=feed&keyword=${query}`,
+      {
+        headers: { Authorization: Authorization }
+      }
+    )
+    .then((response) => {
+      console.log(response.data.data)
+      setResults(response.data.data)
+      navigate("/result");
+    })
+  };
+  
 
-  const handleSearch = (event) => {
+  const handleClick = (event) => {
+    event.preventDefault();
     if (searchQuery !== '') {
-      setResults({});
-      setRecentSearches((prevSearches) => [...prevSearches, searchQuery]);
-      console.log(`${searchQuery}로 검색을 수행합니다.`)
-      // setSearchQuery('');
-      axios.get(
-        `${process.env.REACT_APP_API}/api/search?page=0&type=feed&keyword=${searchQuery}`,
-        {
-          headers: { Authorization: Authorization}
-        }
-      )
-      .then((response)=>{
-        console.log(response.data)
-        setResults(response.data)
-      })
-
-    } else {
-      event.preventDefault();
+      handleSearch(searchQuery);
     }
   };
 
+
   const handleSuggestionClick = (suggestion) => {
     setSearchQuery(suggestion);
+    handleSearch(suggestion);
     setSuggestions([]);
   };
+  
 
   const autocomplete = (value) => {
     setSearchQuery(value);
@@ -54,16 +61,20 @@ const SearchInput = () => {
       headers: { Authorization: Authorization }
     })
     .then((response)=>{
-      console.log(response.data)
-      setSuggestions(response.data.nickname)
+      console.log(response.data.data)
+      setSuggestions(response.data.data.nickname)
     })
   };
   
 
   useEffect(() => {
-    setFromSearchResults(location.pathname.startsWith('/search-results'));
+    if (!location.pathname.startsWith('/result')) {
+      setSearchQuery('');
+    }
+    setFromSearchResults(location.pathname.startsWith('/result'));
   }, [location]);
 
+  
   return (
     <div>
     <div className={styles['input-container']}>
@@ -76,13 +87,11 @@ const SearchInput = () => {
         onChange={(e) => autocomplete(e.target.value)}
         placeholder='검색어를 입력하세요'
       />
-      <Link to="/result"> 
-      <button className={styles['search-btn']} onClick={handleSearch}>
-        <SearchOutlinedIcon className={styles.image}/>
+      <button className={styles['search-btn']} onClick={handleClick}>
+      <SearchOutlinedIcon className={styles.image}/>
       </button>
-      </Link>
     </div>
-    <Autocomplete suggestions={suggestions} onSuggestionClick={handleSuggestionClick} />
+    {suggestions.length > 0 && <Autocomplete suggestions={suggestions} onSuggestionClick={handleSuggestionClick} />}
     </div>
   );
 };
