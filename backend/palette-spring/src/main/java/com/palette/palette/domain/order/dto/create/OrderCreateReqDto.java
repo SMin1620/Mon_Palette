b070214ad0a46432d1fc2984cd650571c3f2afd6
ske1620 <dltmdals1620@gmail.com>
2023-08-02 17:48:14 +0900

    Feat : 주문 생성 하다 맘.

---
+++ backend/palette-spring/src/main/java/com/palette/palette/domain/order/dto/create/OrderCreateReqDto.java
@@ -0,0 +1,16 @@
+package com.palette.palette.domain.order.dto.create;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderCreateReqDto {
+
+
+
+}
