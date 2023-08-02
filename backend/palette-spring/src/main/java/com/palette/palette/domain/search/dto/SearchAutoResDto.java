3e201eb2b9cf8e4d89fa65c7629b6809c3c46219
ske1620 <dltmdals1620@gmail.com>
2023-08-02 15:04:45 +0900

    Fix : 자동완성 유저 데이터 추가

--- backend/palette-spring/src/main/java/com/palette/palette/domain/search/dto/SearchAutoResDto.java
+++ backend/palette-spring/src/main/java/com/palette/palette/domain/search/dto/SearchAutoResDto.java
@@ -1,6 +1,7 @@
 package com.palette.palette.domain.search.dto;
 
 
+import com.palette.palette.domain.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +13,22 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class SearchAutoResDto {
 
-    private String searchKeyword;
+    private Long id;
+
+    private String email;
+
+    private String nickname;
+
+    private String profileImage;
+
+    public static SearchAutoResDto toDto(User user) {
+
+        return SearchAutoResDto.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .nickname(user.getNickname())
+                .profileImage(user.getProfileImage())
+                .build();
+    }
 
 }
