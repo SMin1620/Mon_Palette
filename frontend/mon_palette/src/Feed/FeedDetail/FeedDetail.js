f1a7bb897ba91cc9cf1c9ead6919cb2d37754a00
정수완 <krkdhs6240@naver.com>
2023-08-03 15:57:41 +0900

    Feature: 피드 생성 수정 조회

---
+++ frontend/mon_palette/src/Feed/FeedDetail/FeedDetail.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header/Header";
+import FeedContent from "./FeedContent/FeedContent";
+import Comment from "./Comment/Comment"
+
+function FeedDetail() {
+    return (
+        <div>
+            <Header />
+            <FeedContent />
+            <Comment />
+        </div>
+)}
+export default FeedDetail;
