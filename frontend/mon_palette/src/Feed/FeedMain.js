6aafaea18ad9876d5f2fc5829309e5d75c06ecc8
정수완 <krkdhs6240@naver.com>
2023-08-04 14:23:06 +0900

    Fix: FeedMain.js Feature: FeedMain.js 에 해시태그 추가

--- frontend/mon_palette/src/Feed/FeedMain.js
+++ frontend/mon_palette/src/Feed/FeedMain.js
@@ -8,6 +8,7 @@ import { useNavigate } from 'react-router-dom';
 function FeedMain() {
   const token = useRecoilValue(loginState);
   const [feedInfo, setFeedInfo] = useState([]);
+  const [tagInfo, setTagInfo] = useState([])
   const [feedPage, setFeedPage] = useState(0);
   const [tagState, setTagState] = useState(null);
 
@@ -18,8 +19,7 @@ function FeedMain() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    getFeed(); // axios 요청 보내기
-    console.log('여기 한번만 실행 되야해')
+    // getFeed(); // axios 요청 보내기
     const observer = new IntersectionObserver(handleObs, { threshold: 0.5 }); // 페이지 최초 렌더링시 옵저버 생성
 
     if (obsRef.current) observer.observe(obsRef.current);
@@ -48,9 +48,8 @@ function FeedMain() {
         headers: { Authorization: token }
       });
 
-      // 새로운 데이터를 기존 데이터와 합치기 위해 spread 연산자 사용
       setFeedInfo((prevFeedInfo) => [...prevFeedInfo, ...response.data.data.feeds]);
-      console.log(typeof(response.data.data))
+      setTagInfo(response.data.data.tagRanking)
     } catch (error) {
       console.error(error);
     }
@@ -59,25 +58,26 @@ function FeedMain() {
   const handleFeedDetail = (feedId) => {
     navigate(`/feed/${feedId}`);
   };
-
-  console.log(feedInfo)
-  console.log(feedPage)
   
+  console.log(feedInfo)
 
   return (
     feedInfo&&<div className="feedMain">
       {/* 해시태그 부분 */}
       <div className="feed_tags_container">
         <div className="feed_tags">
-          {feedInfo.map((tag, index) => {
+          {tagInfo.map((tag, index) => {
+            const keyWordLength = tag.keyword.length
+            const tagItemWidth = keyWordLength * 1.5
+
             return (
               <div className="feed_tag_item" onClick={() => {
-                if (tagState && tagState !== tag) {
-                  setTagState(tag);
+                if (tagState && tagState !== tag.keyword) {
+                  setTagState(tag.keyword);
                 } else {
                   setTagState(null);
                 }
-              }} key={index}># {tag.hashTags}
+              }} key={index} style={{ width: `${tagItemWidth}rem`}}># {tag.keyword}
               </div>
             );
           })}
@@ -88,17 +88,22 @@ function FeedMain() {
       <div className="feedMain_body">
         <div className="feedMain_body_info">
           <div className="feedMain_body_container">
-            {feedInfo.map((info, index) => {
-              if (!tagState || info.hashTags.includes(tagState)) {
-                return (
-                  <div className="feedMain_body_info_item" key={index}>
-                    <img src={info.feedImages[0].imagePath} alt="" onClick={() => handleFeedDetail(info.id)} />
+            {
+            feedInfo&&feedInfo.map((info, index) => {
+              return <div className="feedMain_body_info_item" key={index}>
+                <div>
+                  <img src={info.feedImages[0].imagePath} alt="" onClick={() => handleFeedDetail(info.id)} className="feedMain_body_info_item_top" />
+
+                  <div className="feedMain_body_info_item_bottom">
+                    <img src={info.user.profileImage} alt={info.user.name}/>
+
+                    <p>{info.user.nickname}</p>
                   </div>
-                );
-              } else {
-                return null;
-              }
-            })}
+
+                </div>
+              </div>
+              })
+            }
           </div>
         </div>
       </div>
