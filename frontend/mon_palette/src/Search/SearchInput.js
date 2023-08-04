bf98c572a2c74927715ea7ca3666741311431aac
subin <dltmdals1620@gmail.com>
2023-08-04 17:07:45 +0900

    Feat : 검색 기능 보완

--- frontend/mon_palette/src/Search/SearchInput.js
+++ frontend/mon_palette/src/Search/SearchInput.js
@@ -1,32 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
-import { recentSearchesState, searchQueryState } from './Atom';
+import { recentSearchesState } from './Atom';
 import styles from './SearchInput.module.css'
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import ArrowCircleLeftOutlinedIcon from '@mui/icons-material/ArrowCircleLeftOutlined';
 import axios from 'axios';
 import { loginState } from '../user/components/Atom/loginState';
 import { resultsState } from './Atom';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useSearchParams } from 'react-router-dom';
 import Autocomplete from './AutoComplete';
 
 const SearchInput = () => {
-  const [searchQuery, setSearchQuery] = useRecoilState(searchQueryState);
+  // const [recentSearches, setRecentSearches] = useRecoilState(recentSearchesState);
   const Authorization = useRecoilValue(loginState);
-  const [recentSearches, setRecentSearches] = useRecoilState(recentSearchesState);
-  const [results, setResults] = useRecoilState(resultsState);
-  const location = useLocation();
+  // const [results, setResults] = useRecoilState(resultsState);
   const [fromSearchResults, setFromSearchResults] = useState(false);
   const [suggestions, setSuggestions] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
   const navigate = useNavigate();
   const [showSuggestions, setShowSuggestions] = useState(false);
+  
+  const query = searchParams.get('query') || '';
 
   const handleSearch = (query) => {
-    setResults({});
-    setRecentSearches((prevSearches) => [...prevSearches, query]);
+    // setResults({});
+    // setRecentSearches((prevSearches) => [...prevSearches, query]);
     console.log(`${query}로 검색을 수행합니다.`)
     setShowSuggestions(false);
-  
+
     axios.get(
       `${process.env.REACT_APP_API}/api/search?page=0&type=feed&keyword=${query}`,
       {
@@ -34,27 +35,24 @@ const SearchInput = () => {
       }
     )
     .then((response) => {
-      console.log(response.data.data)
-      setResults(response.data.data.feed)
-      navigate("/result");
+      // console.log(response.data.data)
+      // setResults(response.data.data.feed)
+      navigate(`/result?query=${query}`);
     })
   };
-  
 
   const handleClick = (event) => {
     event.preventDefault();
-    if (searchQuery !== '') {
-      handleSearch(searchQuery);
+    if (query !== '') {
+      handleSearch(query);
     }
   };
 
-
   const handleSuggestionClick = (suggestion) => {
-    setSearchQuery(suggestion);
+    setSearchParams({ query: suggestion });
     handleSearch(suggestion);
     setSuggestions([]);
   };
-  
 
   const autocomplete = (value) => {
     if (value) {
@@ -75,17 +73,18 @@ const SearchInput = () => {
       setShowSuggestions(false);
     }
   };
-  
-  
 
   useEffect(() => {
-    if (!location.pathname.startsWith('/result')) {
-      setSearchQuery('');
-    }
-    setFromSearchResults(location.pathname.startsWith('/result'));
-  }, [location]);
+    const path = window.location.pathname;
+    setFromSearchResults(path.startsWith('/result'));
+  }, []);
 
-  
+  useEffect(() => {
+    if (query) {
+      autocomplete(query);
+    }
+  }, [query]);
+
   return (
     <div className={styles.div}>
     <div className={styles['input-container']}>
@@ -94,9 +93,9 @@ const SearchInput = () => {
       </Link>
       <input className={styles['search-input']}
         type="text"
-        value={searchQuery}
+        value={query}
         onChange={(e) => {
-          setSearchQuery(e.target.value);
+          setSearchParams({ query: e.target.value });
           autocomplete(e.target.value); 
           }
         }
