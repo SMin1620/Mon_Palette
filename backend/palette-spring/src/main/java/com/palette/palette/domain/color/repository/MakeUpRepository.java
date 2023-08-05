4a688ad596c8354bcbbb4d6ac3e41425936b7d8e
이승민 <dltmdals1620@gmail.com>
2023-08-05 17:42:38 +0900

    Fix : 메이크업과 컬러 엔티티 중복 제외

--- backend/palette-spring/src/main/java/com/palette/palette/domain/color/repository/MakeUpRepository.java
+++
@@ -1,8 +0,0 @@
-package com.palette.palette.domain.color.repository;
-
-
-import com.palette.palette.domain.color.entity.MakeUp;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MakeUpRepository extends JpaRepository<MakeUp, Long> {
-}
