f165ec11ff4e594d14499112db2540f0bccb7671
이승민 <ibb09@naver.com>
2023-08-06 02:11:57 +0900

    Feat : 주문 생성 관련 Dto 설계

--- backend/palette-spring/src/main/java/com/palette/palette/domain/order/service/OrderService.java
+++ backend/palette-spring/src/main/java/com/palette/palette/domain/order/service/OrderService.java
@@ -1,6 +1,9 @@
 package com.palette.palette.domain.order.service;
 
 
+import com.palette.palette.domain.order.dto.create.OrderCreateReqDto;
+import com.palette.palette.domain.order.entity.Order;
+import com.palette.palette.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,7 +18,8 @@ public class OrderService {
 //     * 주문 생성
 //     */
 //    @Transactional
-//    public void orderCreate(OrderCreateReqDto orderCreateReqDto) {
+//    public void orderCreate(OrderCreateReqDto orderCreateReqDto, User user) {
+//
 //
 //    }
 
