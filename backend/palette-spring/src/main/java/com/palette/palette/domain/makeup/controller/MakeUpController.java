90e2543442779b03b83e78222c6edb5dc62193fe
이승민 <ibb09@naver.com>
2023-08-05 17:51:37 +0900

    Fix : 가상 메이크업 API 수정

--- backend/palette-spring/src/main/java/com/palette/palette/domain/makeup/controller/MakeUpController.java
+++ backend/palette-spring/src/main/java/com/palette/palette/domain/makeup/controller/MakeUpController.java
@@ -1,7 +1,6 @@
 package com.palette.palette.domain.makeup.controller;
 
 import com.palette.palette.common.BaseResponse;
-import com.palette.palette.domain.makeup.dto.makeup.MakeUpReqDto;
 import com.palette.palette.domain.makeup.service.MakeUpService;
 import com.palette.palette.domain.user.entity.User;
 import com.palette.palette.domain.user.repository.UserRepository;
@@ -11,7 +10,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
@@ -60,40 +58,6 @@ public class MakeUpController {
         } catch (Exception e) {
             e.printStackTrace();
             return BaseResponse.error("메이크업 샘플 데이텀 목록 조회 실패");
-        }
-    }
-
-
-    @Operation(summary = "메이크업 이미지 업로드")
-    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public BaseResponse imageUpload(
-            HttpServletRequest request,
-            MakeUpReqDto makeUpReqDto
-    ) {
-        System.out.println("메이크업 이미지 업로드 컨트롤러");
-
-        try {
-            /////////////////////// 토큰으로 인가된 사용자 정보 처리하는 로직
-            String token = jwtTokenProvider.resolveToken(request);
-            jwtTokenProvider.validateToken(token);
-
-            System.out.println("token >>> " + token);
-
-            Authentication authentication = jwtTokenProvider.getAuthentication(token);
-            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-
-            User user = userRepository.findByEmail(userDetails.getUsername()).get();
-
-            // 유저 예외처리 :: 예외처리 커스텀 필요
-            if (user == null) {
-                throw new UserPrincipalNotFoundException("유효한 사용자가 아닙니다.");
-            }
-
-
-            return BaseResponse.success(true);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return BaseResponse.error("메이크업 이미지 업로드 실패");
         }
     }
 }
