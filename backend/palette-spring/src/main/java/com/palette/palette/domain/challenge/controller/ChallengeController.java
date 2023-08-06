71b1e7b6a84fcd73b47f53da29c2dcf9a6693782
이승민 <ibb09@naver.com>
2023-08-06 15:51:50 +0900

    Feat : 퍼스널컬러 팔로우 유저의 챌린지 영상 중 24시간 내에 챌린지만 조회 기능 구현

--- backend/palette-spring/src/main/java/com/palette/palette/domain/challenge/controller/ChallengeController.java
+++ backend/palette-spring/src/main/java/com/palette/palette/domain/challenge/controller/ChallengeController.java
@@ -139,6 +139,41 @@ public class ChallengeController {
 
 
     /**
+     * 챌린지 팔로우 최근 조회
+     */
+    @Operation(summary = "챌린지 팔로우 최근 조회")
+    @GetMapping("/recent")
+    public BaseResponse challengeRecentFollow(
+            HttpServletRequest request
+    ) {
+        System.out.println("챌린지 팔로우 최근 목록 조회");
+
+        try {
+            //////////////////////// 토큰으로 인가된 사용자 정보 처리하는 로직
+            String token = jwtTokenProvider.resolveToken(request);
+            jwtTokenProvider.validateToken(token);
+
+            System.out.println("token >>> " + token);
+
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+            User user = userRepository.findByEmail(userDetails.getUsername()).get();
+
+            // 유저 예외처리 :: 예외처리 커스텀 필요
+            if (user.getId() == null) {
+                throw new UserPrincipalNotFoundException("유효한 사용자가 아닙니다.");
+            }
+
+            return BaseResponse.success(challengeService.recent(user.getEmail()));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return BaseResponse.error("챌린지 팔로우 최근 조회 실패");
+        }
+    }
+
+
+    /**
      * 챌린지 상세 조회
      */
     @Operation(summary = "챌린지 상세 조회")
