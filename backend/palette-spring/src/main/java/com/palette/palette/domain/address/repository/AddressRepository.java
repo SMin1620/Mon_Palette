94cb79a55345b5f1d3744f03c86024ff61c2b452
choiapple <79374648+choiapple@users.noreply.github.com>
2023-08-07 01:28:32 +0900

    Feat : 배송지 crud 기능 추가

--- backend/palette-spring/src/main/java/com/palette/palette/domain/address/repository/AddressRepository.java
+++ backend/palette-spring/src/main/java/com/palette/palette/domain/address/repository/AddressRepository.java
@@ -6,10 +6,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
 
     @Query("select a from Address a where a.user.id = :userId order by a.isMain DESC")
     List<Address> findByUserId(@Param("userId") Long userId);
+
+    @Query("select a from Address a where a.user.id = :userId and a.isMain = :isMain")
+    Optional<Address> findByUserAndIsMain(@Param("userId") Long userId, @Param("isMain") Integer isMain);
+
+    Optional<Address> findById(Long addressId);
+
 
 }
