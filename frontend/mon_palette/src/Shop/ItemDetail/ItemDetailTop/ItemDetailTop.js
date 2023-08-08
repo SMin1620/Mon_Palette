4c712a6e0ab8758ee3b10018a6b064ec9d73787b
김예슬 <pdy051257@naver.com>
2023-08-08 14:20:44 +0900

    design : 아이템디테일

---
+++ frontend/mon_palette/src/Shop/ItemDetail/ItemDetailTop/ItemDetailTop.js
@@ -0,0 +1,34 @@
+import React from "react";
+import styles from "./ItemDetailTop.module.css"
+
+
+
+
+function ItemDetailTop() {
+
+    const ItemDetailData = {
+        thumbnail: [
+            { 
+                id: 1,
+                url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMDLcfN9cl91KV-xNfqxgZo6jPDbIIBvOUOQ&usqp=CAU"
+            },
+            {
+                id: 2, 
+                url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC4cMS0bf1-t86bf-zAA67MXPXsGLrtZcXvQ&usqp=CAU"
+            },
+            {
+                id: 3,
+                url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq296nUsfyp04NukLFkperiSyZouZzrKva5w&usqp=CAU"
+            }
+
+        ],
+        itemName: "롬앤 틴트",
+        discout: 30,
+        price: 20000
+        
+
+    }
+
+}
+
+export default ItemDetailTop
