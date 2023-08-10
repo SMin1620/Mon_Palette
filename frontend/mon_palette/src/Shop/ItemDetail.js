20c8d98197e97ad8bffa620429c957156d29b2e8
yeseul0722 <pdy051257@naver.com>
2023-08-10 11:05:37 +0900

    Feat : 아이템디테일

--- frontend/mon_palette/src/Shop/ItemDetail.js
+++ frontend/mon_palette/src/Shop/ItemDetail.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import styles from './ItemDetail.module.css';
+import ItemDetailTop from './ItemDetail/ItemDetailTop/ItemDetailTop';
+import ItemDetailBottom from './ItemDetail/ItemDetailBottom/ItemDetailBottom';
 
 const ItemDetail = () => {
+    return (
+        <div>
+            <ItemDetailTop />
+            <ItemDetailBottom />
+        </div>
+    )
 
 };
 
