eafe4f022ecf94e01ad3d85090dbb1cb1e58cb18
이승민 <ibb09@naver.com>
2023-08-13 12:05:53 +0900

    Feat : 배포 ssl

--- backend/palette-django/palette/settings.py
+++ backend/palette-django/palette/settings.py
@@ -115,7 +115,7 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': '1234',
         # 'HOST': '127.0.0.1',
-        'HOST': 'db',
+        'HOST': 'mysql',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
