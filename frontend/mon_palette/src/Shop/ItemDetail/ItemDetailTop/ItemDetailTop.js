2aa3ad79cb5fc3f08e92702ed0ff218864324910
yeseul0722 <pdy051257@naver.com>
2023-08-16 05:40:42 +0900

    Feat : 상품갯수제한로직구현

--- frontend/mon_palette/src/Shop/ItemDetail/ItemDetailTop/ItemDetailTop.js
+++ frontend/mon_palette/src/Shop/ItemDetail/ItemDetailTop/ItemDetailTop.js
@@ -13,95 +13,104 @@ import TopButton from "../TopButton/TopButton";
 
 function ItemDetailTop() {
 
-    // const [ItemDetailData, setItemDetailData] = useState("")
+    const [itemDetailData, setItemDetailData] = useState("")
 
-    // const { itemId } = useParams()
-    // const token = useRecoilValue(loginState)
+    const { itemId } = useParams()
+    const token = useRecoilValue(loginState)
 
-    // useEffect(() => {
-    //    axios
-    //    .get(`${process.env.REACT_APP_API}/api/item/option/${itemId}`, {
-    //     headers: { Authorization: token },
-    //    })
-    //    .then ((response) => {
-    //     console.log(response);
-    //     setItemDetailData(response)
-    //    })
-    //    .catch ((err) => {
-    //         console.log(err);
-    //    })
-    // },)
+    useEffect(() => {
+       axios
+       .get(`${process.env.REACT_APP_API}/api/item/detail/1`, {
+        headers: { Authorization: token },
+       })
+       .then ((response) => {
+        console.log(response.data.data);
+        console.log(response.data.data.user.nickname);
+        setItemDetailData(response.data.data)
+        // console.log(ItemDetailData);
+       })
+       .catch ((err) => {
+            console.log(err);
+       })
+    },[])
 
-    const ItemDetailData = {
-        "thumbnail": "https://pbs.twimg.com/media/EZLMS90VcAUXk-z.jpg:large",
-        "id": 1603,
-        "name": "롬앤 틴트",
-        "content": "롬앤 * 산리오 콜라보 상품이에요! 우와앙아아앙 다들 환영해주세요!!",
-        "manufact": "롬앤",
-        "deliveryFee": "Free",
-        "maximum": 3,
-        "createAt": "2023-08-08T15:00:00",
-        "endAt": "2023-08-17T15:00:00",
-        "discount": 30,
-        "price": 20000,
-        "itemOptionDtoList": [
-            {
-                "id": 1604,
-                "optionName": "레드",
-                "stock": 100
-              },
-              {
-                "id": 1605,
-                "optionName": "오렌지",
-                "stock": 100
-              }
-        ],
-        "itemPhotoDtoList": [
-            {
-                "id": 1602,
-                "itemImage": "https://m.romand.co.kr/web/product/big/202306/85702c95740d57f55b23e0edb9dfb64c.jpg",
-            },
-            {
-                "id": 1603,
-                "itemImage": "https://image.oliveyoung.co.kr/uploads/images/goods/550/10/0000/0018/A00000018727204ko.jpg?l=ko"
-            },
-            {
-                "id": 1604,
-                "itemImage": "https://image.oliveyoung.co.kr/uploads/images/goods/550/10/0000/0018/A00000018727203ko.jpg?l=ko"
-            }
-        ],
-        "itemDetailPhotoDtoList": [
-            {
-                "id": 1,
-                "itemImage": "https://m.romand.co.kr/web/upload/NNEditor/20230616/copy-1686899483-EBA1ACEC95A4XEC82B0EBA6ACEC98A4-EC9993EC9790EBB284EAB1B8EC8AA4-1ECB0A8-ED8BB0ECA795_04.jpg",
-            },
-            {
-                "id": 2,
-                "itemImage": "https://m.romand.co.kr/web/upload/NNEditor/20230626/copy-1687747287-teasing_2_18.jpg"
-            },
-            {
-                "id": 3,
-                "itemImage": "https://m.romand.co.kr/web/upload/NNEditor/20230620/teasing_2_9.jpeg"
-            },
-            {
-                "id": 4,
-                "itemImage": "https://m.romand.co.kr/web/upload/NNEditor/20230620/teasing_2_13.jpeg"
-            },
-            {
-                "id": 5,
-                "itemImage": "https://m.romand.co.kr/web/upload/NNEditor/20230616/EBA1ACEC95A4XEC82B0EBA6ACEC98A4-EC9993EC9790EBB284EAB1B8EC8AA4-1ECB0A8-ED8BB0ECA795_0828EC8898ECA09529.jpg"
-            }
+    console.log('안뇽');
+    
+    // console.log(ItemDetailData, "check");
+    // // console.log(ItemDetailData.user.profileImage);
+    // console.log(ItemDetailData.user);
 
-        ],
-        user: {
 
-            "profileImage": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpsrY1SG7leZFYE6nDsRusqHOM4zX2ojNPiw&usqp=CAU",
-            "nickname": "흰둥이",
-            "role": "IF",
-        },
-    }
+    // const itemDetailData = {
+    //     "thumbnail": "https://pbs.twimg.com/media/EZLMS90VcAUXk-z.jpg:large",
+    //     "id": 1603,
+    //     "name": "롬앤 틴트",
+    //     "content": "롬앤 * 산리오 콜라보 상품이에요! 우와앙아아앙 다들 환영해주세요!!",
+    //     "manufact": "롬앤",
+    //     "deliveryFee": "Free",
+    //     "maximum": 3,
+    //     "createAt": "2023-08-08T15:00:00",
+    //     "endAt": "2023-08-17T15:00:00",
+    //     "discount": 30,
+    //     "price": 20000,
+    //     "itemOptionDtoList": [
+    //         {
+    //             "id": 1604,
+    //             "optionName": "레드",
+    //             "stock": 100
+    //           },
+    //           {
+    //             "id": 1605,
+    //             "optionName": "오렌지",
+    //             "stock": 100
+    //           }
+    //     ],
+    //     "itemPhotoDtoList": [
+    //         {
+    //             "id": 1602,
+    //             "itemImage": "https://m.romand.co.kr/web/product/big/202306/85702c95740d57f55b23e0edb9dfb64c.jpg",
+    //         },
+    //         {
+    //             "id": 1603,
+    //             "itemImage": "https://image.oliveyoung.co.kr/uploads/images/goods/550/10/0000/0018/A00000018727204ko.jpg?l=ko"
+    //         },
+    //         {
+    //             "id": 1604,
+    //             "itemImage": "https://image.oliveyoung.co.kr/uploads/images/goods/550/10/0000/0018/A00000018727203ko.jpg?l=ko"
+    //         }
+    //     ],
+    //     "itemDetailPhotoDtoList": [
+    //         {
+    //             "id": 1,
+    //             "itemImage": "https://m.romand.co.kr/web/upload/NNEditor/20230616/copy-1686899483-EBA1ACEC95A4XEC82B0EBA6ACEC98A4-EC9993EC9790EBB284EAB1B8EC8AA4-1ECB0A8-ED8BB0ECA795_04.jpg",
+    //         },
+    //         {
+    //             "id": 2,
+    //             "itemImage": "https://m.romand.co.kr/web/upload/NNEditor/20230626/copy-1687747287-teasing_2_18.jpg"
+    //         },
+    //         {
+    //             "id": 3, 
+    //             "itemImage": "https://m.romand.co.kr/web/upload/NNEditor/20230620/teasing_2_9.jpeg"
+    //         },
+    //         {
+    //             "id": 4,
+    //             "itemImage": "https://m.romand.co.kr/web/upload/NNEditor/20230620/teasing_2_13.jpeg"
+    //         },
+    //         {
+    //             "id": 5,
+    //             "itemImage": "https://m.romand.co.kr/web/upload/NNEditor/20230616/EBA1ACEC95A4XEC82B0EBA6ACEC98A4-EC9993EC9790EBB284EAB1B8EC8AA4-1ECB0A8-ED8BB0ECA795_0828EC8898ECA09529.jpg"
+    //         }
 
-    console.log(ItemDetailData);
+    //     ],
+    //     user: {
+
+    //         "profileImage": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpsrY1SG7leZFYE6nDsRusqHOM4zX2ojNPiw&usqp=CAU",
+    //         "nickname": "흰둥이",
+    //         "role": "IF",
+    //     },
+    // }
+
+    // // console.log(ItemDetailData);
     const settings = {
         dots: true,
         infinite: false,
@@ -121,8 +130,8 @@ function ItemDetailTop() {
         return `${year}-${month}-${day}`;
     };
 
-    const startDay = createDateFormatted(ItemDetailData.createAt)
-    const endDay = createDateFormatted(ItemDetailData.endAt)
+    const startDay = createDateFormatted(itemDetailData.createAt)
+    const endDay = createDateFormatted(itemDetailData.endAt)
 
 
 
@@ -135,7 +144,8 @@ function ItemDetailTop() {
             <div className={styles.thumb_wrap}>
                 <Slider {...settings}>
                     {
-                        ItemDetailData.itemPhotoDtoList.map((image) => (
+                        itemDetailData && (
+                        itemDetailData.itemPhotoDtoList.map((image) => (
                             <div className={styles.thumbnail} key={image.id}>
                                 <img
                                    className={styles.thumb_img}
@@ -143,62 +153,72 @@ function ItemDetailTop() {
                                     alt=""
                                     />
                             </div>
-                        ))
+                        )))
                     }
                 </Slider> 
             </div>
 
+
+            { itemDetailData && (
             <div>
                 <div className={styles.seller_info}>
                     <div>
                         <img
                             className={styles.seller}
-                            src={ItemDetailData.user.profileImage}
+                            src={itemDetailData.user.profileImage}
                             alt=""
                         />
                     </div>
                     <div className={styles.seller_name}>
                         <h5
                         className={styles.h5}>
-                            {ItemDetailData.user.nickname}
+                            {itemDetailData.user.nickname}
                         </h5>
                     </div>
                 </div>
                 <div className={styles.name}>
-                    <p>{ ItemDetailData.name }</p>
+                    <p>{ itemDetailData.name }</p>
                 </div>
             </div>
+            )}
 
+            { itemDetailData && (
             <div className={styles.price_wrap}>
                 <div>
                     <p
-                    className={styles.discount}>{ItemDetailData.discount}%</p>
+                    className={styles.discount}>{itemDetailData.discount}%</p>
                 </div>
                 <div>
                     <p
-                    className={styles.price}>{ItemDetailData.price}</p>
+                    className={styles.price}>{itemDetailData.price}</p>
                 </div>
                 <div>
                     <p className={styles.result_price}>{ 
-                        Math.round(ItemDetailData.price * ((100 - ItemDetailData.discount) * 0.01 )).toLocaleString()} ₩</p>
+                        Math.round(itemDetailData.price * ((100 - itemDetailData.discount) * 0.01 )).toLocaleString()} ₩</p>
                 </div>
             </div>
+            )}
             <div className={styles.delivery_fee}>
-                <p>Delivery Fee: { ItemDetailData.deliveryFee }</p>
+                <p>Delivery Fee: { itemDetailData.deliveryFee }</p>
+            </div>
+            <div className={styles.maximum}>
+                maximum : { itemDetailData.maximum }
             </div>
             <div className={styles.date}>
-                판매 종료 일자 : {endDay}
+                End at : {endDay}
             </div>
-
+            
+            { itemDetailData &&
             <div className={styles.content_wrap}>
                 <div className={styles.content}>
-                    {ItemDetailData.content}
+                    {itemDetailData.content}
                 </div>
             </div>
+            }
 
             <div className={styles.detail_photo_container}>
-                {
-                    ItemDetailData.itemDetailPhotoDtoList.map((image, index) => (
+                {   
+                    itemDetailData && itemDetailData.itemDetailPhotoDtoList.map((image, index) => (
                         <div key={index}>
                             <img
                                 className={styles.detail_photo}
