9e47fe09960f6b2cb0b2eab298443b6a1a90a0c2
이승민 <dltmdals1620@gmail.com>
2023-08-18 01:59:07 +0900

    Fix : 퍼스널 컬러 api uri 수정

--- backend/palette-django/palette/urls.py
+++ backend/palette-django/palette/urls.py
@@ -41,7 +41,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/django/personal', include("image.urls")),
+    path('django/personal', include("image.urls")),
 
 
     # swagger
